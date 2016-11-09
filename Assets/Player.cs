cc7842b7c514ff47f366efcd346eb0b5b26dc770
Paris Morgan <parism9@gmail.com>
2016-11-08 19:08:50 -0500

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public int health;
+	public Vector3 player_position;
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player_position = transform.position;
+	}
+}
