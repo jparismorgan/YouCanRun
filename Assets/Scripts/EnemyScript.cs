313a9d121b2b0f48ddedef05b865212efbe3bbd9
Paris Morgan <parism9@gmail.com>
2016-11-29 17:09:31 -0500

    1st person view controller

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+	//holds the player that we GetComponent() of in Start()
+	GameObject Player;
+	PlayerScript playerScript;
+	public float walk_speed;
+	//how far the enemy can look for the player
+	public float max_sight_distance;
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find("Player");
+		playerScript = Player.GetComponent<PlayerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 location = GetLineOfSight ();
+		print (location);
+
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		Debug.DrawLine(transform.position, transform.position+fwd*10.0f, Color.red);
+
+		//SeekPlayerAtLocation (location);
+	}
+
+	void SeekPlayerAtLocation(Vector3 location){
+		////////////////////////////////
+		//Input: Location
+		//Function: Move the object to the location, or walk around randomly of null location passed
+		//
+		////////////////////////////////
+		return;
+	}
+
+	Vector3 GetLineOfSight(){
+		////////////////////////////////
+		//Checks what is in the zombies line of sight using a raycaster
+		//Uses the players location to determine if anything is in the way of the player and enemy
+		//Return Value: location of player if in line of sight, none if not
+		////////////////////////////////
+
+		//player_class.player_position is the player positon, as set in Player.cs
+		//transform.position is the position of the object this script is assigned to
+		Vector3 ray_direction = playerScript.player_position - transform.position;
+		//hols information about the ray
+		RaycastHit hit;
+		//max distance the ray will travel
+		max_sight_distance = 1000f;
+
+		if (Physics.Raycast (transform.position, ray_direction, out hit, max_sight_distance)) {
+			if (hit.transform == Player.transform) {
+				// enemy can see the player!
+				return hit.point;
+			} else {
+				//enemy cannot see player
+				return Vector3.zero;
+			}
+		} 
+		//enemy cannot see player
+		return Vector3.zero;
+
+	}
+
+}
---
+++ Assets/Scripts/GameInitialization.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInitialization : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CreateScene ();
+		InitializePlayer ();
+		SpawnZombies ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CreateScene(){
+		return;
+	}
+	void SpawnZombies(){
+		//maybe put a countdown timer for the enemies spawning?
+		return;
+	}
+	void InitializePlayer (){
+		return;
+	}
+		
+}
