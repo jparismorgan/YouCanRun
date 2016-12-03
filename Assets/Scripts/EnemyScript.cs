5b7c0f3284cd89fde63cd844cba385dc31f45ba8
Paris Morgan <parism9@gmail.com>
2016-12-03 14:57:14 -0500

    enemy AI

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,23 +5,34 @@ public class EnemyScript : MonoBehaviour {
 	//holds the player that we GetComponent() of in Start()
 	GameObject Player;
 	PlayerScript playerScript;
-	public float walk_speed;
+	public float walkSpeed = 5.0f;
 	//how far the enemy can look for the player
-	public float max_sight_distance;
+	public float maxSightDistance;
 
 	// Use this for initialization
 	void Start () {
 		Player = GameObject.Find("Player");
 		playerScript = Player.GetComponent<PlayerScript>();
 	}
-	
+
+
+	float lerpMoving=0.0f;
 	// Update is called once per frame
 	void Update () {
-		Vector3 location = GetLineOfSight ();
-		print (location);
 
+		//find player
+		Vector3 playerLocation = SearchForPlayer ();
+		print (playerLocation);
+
+		if (playerLocation == Vector3.zero) {	
+			RandomEnemyMovement ();
+		} else { 
+			SeekPlayerAtLocation (playerLocation);
+		}
+			
+		//debug to see front direction of enemy
 		Vector3 fwd = transform.TransformDirection (Vector3.forward);
-		Debug.DrawLine(transform.position, transform.position+fwd*10.0f, Color.red);
+		Debug.DrawLine(transform.position, transform.position+fwd*30.0f, Color.red);
 
 		//SeekPlayerAtLocation (location);
 	}
@@ -32,33 +43,55 @@ public class EnemyScript : MonoBehaviour {
 		//Function: Move the object to the location, or walk around randomly of null location passed
 		//
 		////////////////////////////////
+		float elapsedTime=0.0f;
+
+			if (playerLocation == Vector3.zero) {
+				//no movement}
+			} else {
+				lerpMoving = 0;
+				lerpMoving += Time.deltaTime; 
+				transform.position = Vector3.MoveTowards(transform.position, playerLocation, walkSpeed * lerpMoving);
+				//
+				//			elapsedTime += Time.deltaTime;
+				//			float percentComplete = elapsedTime / 3.0f; //go from 0.0 to 1.0
+				//			float clampedValue=Mathf.Clamp (percentComplete, 0.0f, 1.0f);
+				//			transform.position = Vector3.Lerp (transform.position, playerLocation, clampedValue);
+			}
+
+
+//		elapsedTime += Time.deltaTime;
+//		float percentComplete = elapsedTime / 3.0f; //go from 0.0 to 1.0
+//		float clampedValue=Mathf.Clamp (percentComplete, 0.0f, 1.0f);
+//		transform.position = Vector3.Lerp (startPos, endPos, clampedValue);
+
 		return;
 	}
 
-	Vector3 GetLineOfSight(){
+	Vector3 SearchForPlayer(){
 		////////////////////////////////
 		//Checks what is in the zombie's line of sight using a raycaster
 		//Uses the player's location to determine if anything is in the way of the player and enemy
-		//Return Value: location of player if in line of sight, none if not
+		//Return Value: location of player if in line of sight and in front of the enemy, none if not
 		////////////////////////////////
 
-		//player_class.player_position is the player positon, as set in Player.cs
+		//player_class.player_position is the player positon, as set in PlayerScript.cs
 		//transform.position is the position of the object this script is assigned to
 		Vector3 ray_direction = playerScript.player_position - transform.position;
 		//holds information about the ray
 		RaycastHit hit;
 		//max distance the ray will travel
-		max_sight_distance = 1000f;
+		maxSightDistance = 1000f;
 
-		if (Physics.Raycast (transform.position, ray_direction, out hit, max_sight_distance)) {
+		if (Physics.Raycast (transform.position, ray_direction, out hit, maxSightDistance)) {
+		//successull raycast will put information into hit
 			if (hit.transform == Player.transform) {
-				// enemy can see the player!
-				return hit.point;
-			} else {
-				//enemy cannot see player
-				return Vector3.zero;
+				//enemy can see the player
+				if (hit.transform.position.z > -0.2f) {
+					//the player is in front of the enemy
+					return hit.point;
+				}
 			}
-		} 
+		}
 		//enemy cannot see player
 		return Vector3.zero;
 
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -12,6 +12,9 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//set player position so that EnemyScript.cs can use it
 		player_position = transform.position;
+
+
 	}
 }
